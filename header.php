cf2ba9e77efb3b83cca2d8fd5631c5887a2c6169
Paweł Szlendak <theevagor@gmail.com>
2017-03-27 17:29:23 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#">
+	<head>
+		<meta property="og:title" content="Podsumowanie partii pokerowej - evagor.pl" />
+		<meta property="og:image" content="http://poker.evagor.pl/assets/thumbnail.jpg" />  
+		<meta property="og:url" content="poker.evagor.pl"/> 
+		
+		<link rel="image_src" href="http://poker.evagor.pl/assets/thumbnail.jpg" /> 
+		<title>Podsumowanie partii pokerowej - evagor.pl</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+		<meta charset="utf-8">
+		<meta name="keywords" content="personal, social, media, pawel, szlenda">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400&amp;subset=latin-ext" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400&amp;subset=latin-ext" rel="stylesheet">  
+	</head>
+	
+	<body>
+		   
+		<script>
+		  window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '240180609751415',
+		      xfbml      : true,
+		      version    : 'v2.8'
+		    });
+		  };
+
+		  (function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "//connect.facebook.net/en_US/sdk.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		</script>
+
+<div id="title">
+	Pokerowe Podsumowanie Partii
+	<div id="menu">
+		<ul id="ulmenu">
+			<li id="limenu"><a href="/">Home</a></li>
+			<li id="limenu"><a href="/rules.php">Zasady</a></li>
+			<li id="limenu"><a target="_blank" href="https://docs.google.com/spreadsheets/d/1_vqB-8NEly0VfJZ0NkbhsJYxKGMJ_PHPee7AR8GTeO4/edit?usp=sharing">Achivementy</a></li>
+			<li id="limenu"><a href="/dodajpartie.php">DodajWynik</a></li>
+		</ul>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -61,13 +61,30 @@
 		
 	}//koniec funkcji setDataBase
 	
-	function showDataBase($mainDataBase, $dateOfGame, $player){
+	function showNameTable($mainDataBase, $dateOfGame, $player){
+		echo "<table id='nametable'>";
+			echo "<tr id='header-vol'>";
+				echo "<tr id='gorna-belka'>";
+					echo "<td id='gorny-lewy-rog-vol'>Imię/DataImię DataImię/Data</td>";
+				echo "</tr>";	
+				echo "<tr id='gorna-belka'>";
+					echo "<td id='gorny-lewy-rog'>Imię/Data</td>";
+				echo "</tr>";	
+					for ($i = 0; $i < count($player); $i++){
+						echo "<tr class='linia-gracza'>";
+							echo "<td class='imie-gracza'>".$player[$i]."</td>";				
+						}
+			echo "</tr>";
+		echo "</table>";
+	}
+	
+	function showDataBase($mainDataBase, $dateOfGame, $player){												
 		echo "<table id='bazadanych'>";
 			echo "<tr rowspan=2 id='header'>";
 				echo "<tr colspan=3 id='gorna-belka'>";
-					echo "<td rowspan=2 id='gorny-lewy-rog'>Imię/Data</td>";
-					for ($i = 0; $i < count($dateOfGame); $i++){
-						echo "<td colspan=3 class='gorna-belka-daty'>".$dateOfGame[$i]."</td>";
+					//echo "<td rowspan=2 id='gorny-lewy-rog'>Imię/Data</td>";
+					for ($i = count($dateOfGame); $i > 0; $i--){
+						echo "<td colspan=3 class='gorna-belka-daty'>".$dateOfGame[$i-1]."</td>";
 					}
 				echo "</tr>";
 					
@@ -80,12 +97,13 @@
 				echo "</tr>";	
 			echo "</tr>";	
 				for ($i = 0; $i < count($player); $i++){
-					echo "<tr class='linia-gracza'><td class='imie-gracza'>".$player[$i]."</td>";
-					for ($j = 0; $j < count($dateOfGame); $j++){
-						echo "<td class='wygrana'>".$mainDataBase[$i][$j]->wygrana."</td>";
-						echo "<td class='pula'>".$mainDataBase[$i][$j]->pula."</td>";
-						echo "<td class='nd'>".$mainDataBase[$i][$j]->nd."</td>";
-					}
+					echo "<tr class='linia-gracza'>";
+						//echo "<td class='imie-gracza'>".$player[$i]."</td>";
+						for ($j = count($dateOfGame); $j > 0; $j--){
+							echo "<td class='wygrana'>".$mainDataBase[$i][$j-1]->wygrana."</td>";
+							echo "<td class='pula'>".$mainDataBase[$i][$j-1]->pula."</td>";
+							echo "<td class='nd'>".$mainDataBase[$i][$j-1]->nd."</td>";
+						}
 					echo "</tr>";	
 				}
 		echo "</table>";
@@ -96,6 +114,15 @@
 		
 		for ($j = 0; $j < count($dateOfGame); $j++){
 			$playerMonay += $mainDataBase[$idGracza][$j]->wygrana;
+		}
+		return $playerMonay;
+	}
+	
+	function playerND($mainDataBase, $dateOfGame, $idGracza){
+		$playerMonay = 0;
+		
+		for ($j = 0; $j < count($dateOfGame); $j++){
+			$playerMonay += $mainDataBase[$idGracza][$j]->nd;
 		}
 		return $playerMonay;
 	}
@@ -148,13 +175,15 @@
 		echo "<table id='personal-ranking-table'>";
 			echo "<tr id='personal-ranking-header-row'><td id='personal-ranking-header-name'>Osobisty ranking</td>";
 			echo "<td>Suma wygranej</td>";
-			echo "<td>Obecność</td></tr>";
+			echo "<td>Obecność</td>";
+			echo "<td>ND</td></tr>";
 			//echo "<td>Wygrane</td></tr>";
 			for ($i = 0; $i < count($player); $i++){
 				echo "<tr class='personal-ranking-name'>";
 					echo"<td class='pr-td-name'>".$player[$i]."</td>";
 					echo "<td class='suma'>".playerWholeMoney($mainDataBase, $dateOfGame, $i)."</td>";
 					echo "<td class='obecnosc'>".playerPresent($mainDataBase, $dateOfGame, $i)."%</td>";
+					echo "<td class='ND'>".playerND($mainDataBase, $dateOfGame, $i)."</td>";
 					//echo "<td class='wygrane'>".playerWins($mainDataBase, $dateOfGame, $i)."</td>";
 				echo "</tr>";	
 			}
@@ -166,7 +195,10 @@
 		$playerSort = $player;
 		
 		for ($i = 0; $i < count($player); $i++){ //stworzenie tablicy z ratio
-			$playerMonayArray[$i] = round(playerWholeMoney($mainDataBase, $dateOfGame, $i)/playerWholeInside($mainDataBase, $dateOfGame, $i), 2);
+			$whole = playerWholeMoney($mainDataBase, $dateOfGame, $i);
+			$inside = playerWholeInside($mainDataBase, $dateOfGame, $i);
+			$nd = playerND($mainDataBase, $dateOfGame, $i);
+			$playerMonayArray[$i] = round(($whole-($nd*600))/$inside, 2);
 		}
 		
 		
@@ -182,7 +214,7 @@
 		
 		echo "<table id='global-ranking-table'>";
 			echo "<tr id='global-ranking-header-row'><td id='global-ranking-header-name'>Globalny ranking</td>";
-			echo "<td>Ratio wygrana/wkład</td>";
+			echo "<td>Ratio [(wygrana-ND*600)/wkład]</td>";
 			for ($i = 0; $i < count($player); $i++){
 				$var = $i + 1;
 				echo "<tr class='global-ranking-name'>";
@@ -196,44 +228,12 @@
 	setDataBase();
 ?>
 
-<!DOCTYPE html>
-<html prefix="og: http://ogp.me/ns#">
-	<head>
-		<meta property="og:title" content="Podsumowanie partii pokerowej - evagor.pl" />
-		<meta property="og:image" content="http://poker.evagor.pl/assets/thumbnail.jpg" />  
-		<meta property="og:url" content="poker.evagor.pl"/> 
-		
-		<link rel="image_src" href="http://poker.evagor.pl/assets/thumbnail.jpg" /> 
-		<title>Podsumowanie partii pokerowej - evagor.pl</title>
-		<link rel="stylesheet" href="style.css" type="text/css">
-		<meta charset="utf-8">
-		<meta name="keywords" content="personal, social, media, pawel, szlenda">
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400&amp;subset=latin-ext" rel="stylesheet"> 
-		<link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400&amp;subset=latin-ext" rel="stylesheet">  
-	</head>
-	
-	<body>
-		   
-		<script>
-		  window.fbAsyncInit = function() {
-		    FB.init({
-		      appId      : '240180609751415',
-		      xfbml      : true,
-		      version    : 'v2.8'
-		    });
-		  };
+<?php include("header.php"); ?>
 
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "//connect.facebook.net/en_US/sdk.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-		</script>
-		   
-		<div id="title">Pokerowe Podsumowanie Partii</div>
 		<div id="container">
+			<div id="div-names">
+				<? showNameTable($mainDataBase, $dateOfGame, $player); ?>
+			</div>
 			<div id="div-table">
 				<? showDataBase($mainDataBase, $dateOfGame, $player); ?>
 			</div>
@@ -243,5 +243,5 @@
 			<div id="global-ranking" class="ranking"><? globalRanking($mainDataBase, $dateOfGame, $player);?></div>
 			<div id="game-ranking" class="ranking">Podsumowanie gier</div>
 		</div>
-	</body>
-</html>
+		
+<?php include("footer.php"); ?>		
